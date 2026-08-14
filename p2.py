ea444281b3899ebfa9214af432ca60622e68fbfe
Shikhaaax <shikhasingh032004@gmail.com>
2026-08-14 14:05:34 +0530

    Author2 - added grade calculation

---
+++ p2.py
@@ -0,0 +1,12 @@
+# p2.py
+# Author 2
+
+def calculate_grade(total):
+    if total >= 270:
+        return "A"
+    elif total >= 240:
+        return "B"
+    elif total >= 180:
+        return "C"
+    else:
+        return "F"
